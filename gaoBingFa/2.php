91eff02d66dd41ebe54c948998ee3846e035f993
179355991@qq.com <179355991@qq.com>
2020-03-19 14:50:35 +0800

    fix

--- gaoBingFa/2.php
+++ gaoBingFa/2.php
@@ -5,11 +5,11 @@ $redis->connect('127.0.0.1', 6379);
 $redis->auth(123456);
 $llen = 120;// 队列固定的长度
 $key = 'lock';// 锁的名字
+if ($redis->lLen('ooo') + 1 > $llen) {
+    echo '队列已满';
+    exit;
+}
 while (true) {
-    if ($redis->lLen('ooo') + 1 > $llen) {// 初次进来的判断
-        echo '队列已满';
-        break;
-    }
     $redis->set($key, 1);
     //设置键的过期时间
     $redis->setTimeout($key, 1);// 锁的有效时间  1秒
