fb9ee3e509022e3c017d0fd36b750e3c5b970b77
179355991@qq.com <179355991@qq.com>
2020-03-19 13:27:47 +0800

    fix

--- gaoBingFa/2.php
+++ gaoBingFa/2.php
@@ -1,21 +1,19 @@
 <?php
-//echo 13;
-//
-
-
-// file_put_contents("client_1.txt", "client_1 execute:".time()."\r\n",FILE_APPEND);
-// echo 'client_1 execute';
 ini_set("display_errors", "off");
 $redis = new Redis();
-$redis->connect('127.0.0.1',6379);
+$redis->connect('127.0.0.1', 6379);
 $redis->auth(123456);
-
 
 if ($redis->lLen('ooo') >= 100) {// 大于或者等于xxx 就提示队列已满 固定队列长度
     echo '队列已满';
     exit;
 }
 
-$int = $redis->lPush('ooo', '1');
-echo $int;
+if (!$redis->exists('sss')) {
+    $redis->set('sss', 1,1000);
+} else {
+    $int = $redis->lPush('ooo', '1');
+    echo $int;
+}
+
 
