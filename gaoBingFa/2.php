a0767941942c1643cb87799997ed91f6f9aafe95
179355991@qq.com <179355991@qq.com>
2020-03-19 14:21:51 +0800

    fix

--- gaoBingFa/2.php
+++ gaoBingFa/2.php
@@ -3,6 +3,7 @@ $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 $redis->auth(123456);
 
+
 while (true) {
 
     $key = 'sss';
@@ -10,9 +11,14 @@ while (true) {
     //设置键的过期时间
     $redis->setTimeout($key, 1);
     if ($redis->exists('sss')) {
+        if ($redis->lLen('ooo') >= 100) {// 大于或者等于xxx 就提示队列已满 固定队列长度
+            echo '队列已满';
+            break;
+        }
         $redis->incr('num');
         $int = $redis->lPush('ooo', '1');
-        if ($int ==100) {   
+        if ($int >= 100) {
+            echo $int;
             break;
         }
     } else {
