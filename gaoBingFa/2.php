6c1bf667de89df40aab6711c9dcbac7c618e678d
179355991@qq.com <179355991@qq.com>
2020-03-19 14:46:20 +0800

    fix

--- gaoBingFa/2.php
+++ gaoBingFa/2.php
@@ -3,19 +3,20 @@ ini_set("display_errors", "off");
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 $redis->auth(123456);
+
 while (true) {
-    if ($redis->lLen('ooo') + 1 > 120) {// 初次访问，如果已满
+    if ($redis->lLen('ooo') + 1 > 120) {// 大于或者等于xxx 就提示队列已满 固定队列长度
         echo '队列已满';
         break;
     }
-    //设置键的过期时间
-    $key = 'lock';
+    $key = 'sss';
     $redis->set($key, 1);
-    $redis->setTimeout($key, 1);// 有效期1秒
-    if ($redis->exists('sss')) {
+    //设置键的过期时间
+    $redis->setTimeout($key, 1);
+    if ($redis->exists($key)) {
 
         $int = $redis->lPush('ooo', '1');
-        if ($int + 1 > 120) {// 队列长度为120 就提示队列已满，退出循环 固定队列长度的作用
+        if ($int + 1 > 120) {// 大于或者等于xxx 就提示队列已满 固定队列长度
             echo $int;
             echo '队列已满';
             break;
