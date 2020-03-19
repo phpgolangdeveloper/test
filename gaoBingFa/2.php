ab2485b83085ae4ce738ea109f26a250ae0c3f6e
179355991@qq.com <179355991@qq.com>
2020-03-19 14:44:25 +0800

    fix

--- gaoBingFa/2.php
+++ gaoBingFa/2.php
@@ -12,16 +12,13 @@ while (true) {
     $key = 'lock';
     $redis->set($key, 1);
     $redis->setTimeout($key, 1);// 有效期1秒
-    if ($redis->exists('lock')) { // 拿到锁
+    if ($redis->exists('sss')) {
         $int = $redis->lPush('ooo', '1');
-        if ($int && $int + 1 > 120) {// 队列长度为120 就提示队列已满，退出循环 固定队列长度的作用
+        if ($int + 1 > 120) {// 队列长度为120 就提示队列已满，退出循环 固定队列长度的作用
             echo '队列已满';
             break;
-        } else {
-            echo '设置队列失败';
-            break;
         }
-    } else {// 没拿到锁的用户，退出
+    } else {
         break;
     }
 }
