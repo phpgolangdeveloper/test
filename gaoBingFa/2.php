2651f2c99b0661eb9c12dc8514236095eead191d
179355991@qq.com <179355991@qq.com>
2020-03-19 14:06:58 +0800

    fix

--- gaoBingFa/2.php
+++ gaoBingFa/2.php
@@ -15,7 +15,7 @@ while (true) {
     if ($redis->exists('sss')) {
         if ($redis->lLen('ooo') >= 100) {// 大于或者等于xxx 就提示队列已满 固定队列长度
             echo '队列已满';
-            exit;
+            break;
         }
         $redis->incr('num');
         $int = $redis->lPush('ooo', '1');
