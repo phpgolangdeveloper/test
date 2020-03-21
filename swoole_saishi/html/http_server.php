9951f04f9698605f983f3b362e346c1153ab303d
179355991@qq.com <179355991@qq.com>
2020-03-22 00:41:20 +0800

    fix;

---
+++ swoole_saishi/html/http_server.php
@@ -0,0 +1,38 @@
+<?php
+include './zhibo.php';
+
+class http_server
+{
+
+    public function __construct()
+    {
+        $http = new swoole_http_server('0.0.0.0', 8811);
+
+        $http->set(
+            [
+                'enable_static_handler' => true,//开启静态文件请求处理功能，需配合 document_root 使用 默认 false
+                'document_root' => '/home/wwwroot/default/twj/swoole/data',//设置静态处理器的路径。类型为数组，默认不启用
+                'worker_num' => 5,
+            ]
+        );
+
+        $http->on('request', 'onRequest');
+        $http->start();
+    }
+
+    public function onRequest($request, $response)
+    {
+
+        ob_start();
+        $zhibo = new zhibo();
+        $zhibo->detaile();
+        $res = ob_get_contents();
+        ob_end_clean();
+        // 如果要把数据放在浏览器，就要用end()这个方法,而且必须要是string
+        $response->end($res);
+
+    }
+
+}
+
+new http_server();
---
+++ swoole_saishi/html/zhibo.php
@@ -0,0 +1,47 @@
+<?php
+
+class zhibo
+{
+
+    /***
+     * 登录界面
+     */
+    public function loginHtml()
+    {
+
+    }
+    /***
+     * 登录操作
+     */
+    public function login()
+    {
+
+    }
+
+
+    /***
+     * 发送验证码
+     */
+    public function send()
+    {
+
+    }
+
+    /***
+     * 验证码验证
+     */
+    public function isCode() {
+
+    }
+
+
+    /***
+     * 直播详情页
+     */
+    public function detaile()
+    {
+        echo 1;
+    }
+
+}
+
