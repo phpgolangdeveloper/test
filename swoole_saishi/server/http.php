f783fcb9f3390e4fe72e4512ab72d09c87bd8bee
179355991@qq.com <179355991@qq.com>
2020-03-22 02:45:36 +0800

    fix;

--- swoole_saishi/server/http.php
+++ swoole_saishi/server/http.php
@@ -40,7 +40,7 @@ class Http
         var_dump($request->get);
         $class =  $request->get['class'];
         $func = $request->get['func'];
-        new ($class())->$func();
+        (new $class())->$func();
         $res = ob_get_contents();
         ob_end_clean();
 
