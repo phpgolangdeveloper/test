6f452df67ccfb98919e75ed47167406eea8da6f3
179355991@qq.com <179355991@qq.com>
2020-03-29 12:16:30 +0800

    idx

--- swoole_saishi/server/admin.php
+++ swoole_saishi/server/admin.php
@@ -1,11 +1,18 @@
 <?php
 
-class admin {
+class admin
+{
 
-    public function image() {
+    public function image()
+    {
 
         print_r($_FILES);
 
     }
 
+    public function live()
+    {
+        print_r($_POST);
+
+    }
 }
