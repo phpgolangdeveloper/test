35f056429eb13983b5bb27bbd3c849170e591870
179355991@qq.com <179355991@qq.com>
2020-03-29 15:14:33 +0800

    idx

--- swoole_saishi/server/admin.php
+++ swoole_saishi/server/admin.php
@@ -17,6 +17,14 @@ class admin
         // 1 把赛况基本信息入库
 
         // 2 把数据组装好 push 到直播页面
-        $_POST['http_server']->push(1, 'hello');
+
+        $clients = redisTest::getObj()->sMembers('live_game_key');
+
+        print_r($clients);
+        foreach ($clients as $fd) {
+        $_POST['http_server']->push($fd, 'hello');
+        }
+
+//        $_POST['http_server']->push(1, 'hello');
     }
 }
